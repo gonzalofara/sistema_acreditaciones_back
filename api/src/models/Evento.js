0828d917a0b0b372fa0bfa90eaf738e671d4c30b
Gonzalo <fonsofara@gmail.com>
2022-10-17 13:06:32 -0300

    actualizando

--- api/src/models/Evento.js
+++ api/src/models/Evento.js
@@ -1,24 +1,17 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-<<<<<<< HEAD:api/src/models/Type.js
   sequelize.define(
     "Type",
     {
-      name: {
+      cliente: {
         type: DataTypes.STRING,
+        allowNull: false,
       },
-=======
-    sequelize.define('Type', {
-        cliente: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        nombre:{
-            type: DataTypes.STRING,
-            allowNull:false,
-        }
->>>>>>> f7117088f01dfaa0c5700342a12a1ec04d76fbe6:api/src/models/Evento.js
+      nombre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
     {
       timestamps: false,
